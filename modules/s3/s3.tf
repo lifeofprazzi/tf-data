559676e90c04ac01e29e81935f246b986a1ce670
jitu <jitu@mail.com>
2026-01-20 13:22:04 +0530

    Region parameter added to s3 module

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket" "s3" {
 	bucket = var.bucket-name
+	region = var.region-name
 
 }
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,3 +1,7 @@
 variable "bucket-name" {
 	description = "Your S3 Bucket name, make sure bucket name should name duplicate"
+}
+
+variable "region-name" {
+	description = "YOUR S3 BUCKET REGION NAME"
 } 
